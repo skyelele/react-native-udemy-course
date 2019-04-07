7bb0cb4f8d27e51a8f3ce7292bb5b909310fb068
Skye Leahy <skyeleahy@protonmail.com>
2019-04-07 12:24:12 -0700

    initial commit for tech_stack app. added practice file to get familiar with redux

---
+++ tech_stack/practice.js
@@ -0,0 +1,32 @@
+// https://stephengrider.github.io/JSPlaygrounds/
+
+const reducer = (state = [], action) => {
+  if (action.type === "split_string") {
+    return action.payload.split("");
+  } else if (action.type === "add_character") {
+    // state.push(action.payload);
+    // return state;
+    return [...state, action.payload];
+  }
+
+  return state;
+};
+
+const store = Redux.createStore(reducer);
+
+store.getState();
+
+const action = {
+  type: "split_string",
+  payload: "asdf"
+};
+
+store.dispatch(action);
+store.getState();
+
+const action2 = {
+  type: "add_Character",
+  payload: "a"
+};
+
+store.dispatch(action2);
