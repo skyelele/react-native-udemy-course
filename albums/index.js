ad4a777448a9341a01cf8975e1864ff928760fb0
Skye Leahy <skyeleahy@protonmail.com>
2019-04-06 20:24:29 -0700

    add componentwillmount code, add initial state code for albumlist component

--- albums/index.js
+++ albums/index.js
@@ -1,11 +1,17 @@
 // Import a library to help create a component
 import React from "react";
-import { AppRegistry } from "react-native";
+import { AppRegistry, View } from "react-native";
 import Header from "./src/components/header";
+import AlbumList from "./src/components/AlbumList";
 
 // Create component
 
-const App = () => <Header headerText={"Albums!"} />;
+const App = () => (
+  <View>
+    <Header headerText={"Albums!"} />
+    <AlbumList />
+  </View>
+);
 
 // REnder it to the device
 AppRegistry.registerComponent("albums", () => App);
