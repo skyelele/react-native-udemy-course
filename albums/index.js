e7d84de215103c891cbaa76c490af49465b18b86
Skye Leahy <skyeleahy@protonmail.com>
2019-04-06 19:26:55 -0700

    add header component, props code

--- albums/index.js
+++ albums/index.js
@@ -1,7 +1,11 @@
 // Import a library to help create a component
 import React from "react";
-import ReactNative from "react-native";
+import { AppRegistry } from "react-native";
+import Header from "./src/components/header";
 
 // Create component
 
+const App = () => <Header headerText={"Albums!"} />;
+
 // REnder it to the device
+AppRegistry.registerComponent("albums", () => App);
