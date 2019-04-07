76d8a1ecd80e21ee84a0c653445654c90c0e628f
Skye Leahy <skyeleahy@protonmail.com>
2019-04-06 22:03:00 -0700

    implement styling, dividing cards into sections, passing components as props

--- albums/src/components/AlbumList.js
+++ albums/src/components/AlbumList.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
-import { View, Text } from "react-native";
+import { View } from "react-native";
 import axios from "axios";
+import AlbumDetail from "./AlbumDetail";
 
 class AlbumList extends Component {
   state = { albums: [] };
@@ -8,15 +9,19 @@ class AlbumList extends Component {
   componentWillMount() {
     axios
       .get("https://rallycoding.herokuapp.com/api/music_albums")
-      .then(response => this.setState({ albums: response.data });
+      .then(response => this.setState({ albums: response.data }));
+  }
+
+  renderAlbums() {
+    return this.state.albums.map(album => (
+      <AlbumDetail key={album.title} album={album} />
+    ));
   }
 
   render() {
-    return (
-      <View>
-        <Text>Album List!!!</Text>
-      </View>
-    );
+    console.log(this.state);
+
+    return <View>{this.renderAlbums()}</View>;
   }
 }
 
