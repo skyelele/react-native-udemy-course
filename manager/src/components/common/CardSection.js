9a1c181078ba9e69649bd248c81555ddc76cedd3
Skye Leahy <skyeleahy@protonmail.com>
2019-04-08 11:23:27 -0700

    update employee create component, implement new styles to that comp.

--- manager/src/components/common/CardSection.js
+++ manager/src/components/common/CardSection.js
@@ -2,7 +2,9 @@ import React from "react";
 import { View } from "react-native";
 
 const CardSection = props => {
-  return <View style={styles.containerStyle}>{props.children}</View>;
+  return (
+    <View style={[styles.containerStyle, prop.style]}>{props.children}</View>
+  );
 };
 
 const styles = {
