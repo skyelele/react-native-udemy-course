be6a143ccf9635b73c7d5119cb513ed56084af7b
Skye Leahy <skyeleahy@protonmail.com>
2019-04-08 10:38:28 -0700

    add employeecreatelist component, action, and reducer, refactor action creators, implement dynamic property updates

---
+++ manager/src/Router.js
@@ -0,0 +1,41 @@
+import React, { Component } from "react";
+import { Scene, Router, Actions } from "react-native-router-flux";
+import LoginForm from "./components/LoginForm";
+import EmployeeList from "./components/EmployeeList";
+import EmployeeCreate from "./components/EmployeeCreate";
+
+class Router extends Component {
+  render() {
+    return (
+      <Router>
+        <Scene key="root" hideNavBar>
+          <Scene key="auth">
+            <Scene
+              key="login"
+              component={LoginForm}
+              title="Please Login"
+              initial
+            />
+          </Scene>
+          <Scene key="main">
+            <Scene
+              rightTitle="Add"
+              onRight=[() => Actions.employeeCreate()]
+              key="employeeList"
+              component={EmployeeList}
+              title="Employees"
+              initial
+            />
+            <Scene
+              key="employeeCreate"
+              component={EmployeeCreate}
+              title="Create Employee"
+            />
+          </Scene>
+        </Scene>
+      </Router>
+    );
+  }
+}
+
+export default Router;
---
+++ manager/src/components/EmployeeCreate.js
@@ -0,0 +1,51 @@
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import { employeeUpdate } from "../actions";
+import { Card, CardSection, Input, Button } from "./common";
+
+class EmployeeCreate extends Component {
+  render() {
+    return (
+      <Card>
+        <CardSection>
+          <Input
+            label="Name"
+            placeholder="Jane"
+            value={this.props.name}
+            onChangeText={value =>
+              this.props.employeeUpdate({ prop: "name", value })
+            }
+          />
+        </CardSection>
+
+        <CardSection>
+          <Input
+            label="Phone"
+            placeholder="555-555-5555"
+            value={this.props.phone}
+            onChangeText={value =>
+              this.props.employeeUpdate({ prop: "phone", value })
+            }
+          />
+        </CardSection>
+
+        <CardSection />
+
+        <CardSection>
+          <Button>Create</Button>
+        </CardSection>
+      </Card>
+    );
+  }
+}
+
+const mapStateToProps = state => {
+  const { name, phone, shift } = state.employeeForm;
+
+  return { name, phone, shift };
+};
+
+export default connect(
+  mapStateToProps,
+  { employeeUpdate }
+)(EmployeeCreate);
