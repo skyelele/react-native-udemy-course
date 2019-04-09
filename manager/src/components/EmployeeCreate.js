2f245446c788f3f50c01cc2b1c9f6660abdd0c48
Skye Leahy <skyeleahy@protonmail.com>
2019-04-08 17:07:55 -0700

    implement employeeCreate action+reducer, also, add firebase code to store name/phone/shift into database and redirect user upon that data storage completion'

--- manager/src/components/EmployeeCreate.js
+++ manager/src/components/EmployeeCreate.js
@@ -1,10 +1,16 @@
 import React, { Component } from "react";
 import { Picker, Text } from "react-native";
 import { connect } from "react-redux";
-import { employeeUpdate } from "../actions";
+import { employeeUpdate, employeeCreate } from "../actions";
 import { Card, CardSection, Input, Button } from "./common";
 
 class EmployeeCreate extends Component {
+  onButtonPress() {
+    const { name, phone, shift } = this.props;
+
+    this.props.employeeCreate({ name, phone, shift: shift || "Monday" });
+  }
+
   render() {
     return (
       <Card>
@@ -30,8 +36,8 @@ class EmployeeCreate extends Component {
           />
         </CardSection>
 
-        <CardSection>
-          <Text style={StyleSheet.pickerTextStyle}>Shift</Text>
+        <CardSection style={{ flexDirection: "column" }}>
+          <Text style={styles.pickerTextStyle}>Shift</Text>
           <Picker
             style={{ flex: 1 }}
             selectedValue={this.props.shift}
@@ -50,12 +56,19 @@ class EmployeeCreate extends Component {
         </CardSection>
 
         <CardSection>
-          <Button>Create</Button>
+          <Button onPress={this.onButtonPress.bind(this)}>Create</Button>
         </CardSection>
       </Card>
     );
   }
 }
+
+const styles = {
+  pickerTextStyle: {
+    fontSize: 18,
+    paddingLeft: 20
+  }
+};
 
 const mapStateToProps = state => {
   const { name, phone, shift } = state.employeeForm;
@@ -65,5 +78,5 @@ const mapStateToProps = state => {
 
 export default connect(
   mapStateToProps,
-  { employeeUpdate }
+  { employeeUpdate, employeeCreate }
 )(EmployeeCreate);
